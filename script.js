5aa341d0f38d3afebcbd780bf970172f1cf7789a
Emmanuel Orubele <blontf@gmail.com>
2018-11-21 13:45:49 -0500

    New file for my javaScript

---
+++ script.js
@@ -0,0 +1,14 @@
+var firstName = 'Emmanuel';
+console.log(firstName);
+var lastName = 'Smith';
+var age = 28; 
+
+var fullAge = false;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = age
+
+console.log(job);
